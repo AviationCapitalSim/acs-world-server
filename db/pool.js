8dbe7ea247f728a6a320f913129194f778fac08d
AviationCapitalSim <aviationcapitalsim@gmail.com>
2026-03-05 11:45:55 +0600

    Update pool.js
    import pg from "pg";
    import dotenv from "dotenv";
    
    dotenv.config();
    
    const { Pool } = pg;
    
    if (!process.env.DATABASE_URL) {
      throw new Error("Missing DATABASE_URL in environment");
    }
    
    export const pool = new Pool({
      connectionString: process.env.DATABASE_URL,
      ssl: {
        rejectUnauthorized: false
      },
      max: 20
    });

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,6 @@
 import pg from "pg";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const { Pool } = pg;
@@ -10,5 +11,8 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
   max: 20
 });
