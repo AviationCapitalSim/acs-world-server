b9e7dd78f56c57cda52ffbfa741a8b06d764bc97
AviationCapitalSim <aviationcapitalsim@gmail.com>
2026-03-21 12:44:00 +0600

    Update server.js
    app.use(cors({

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const app = express();
 app.set("trust proxy", 1);
 
 app.use(cors({
-  origin: "*",
+  origin: "https://aviationcapitalsim.github.io",
+  credentials: true,
   methods: ["GET","POST","PATCH","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"]
 }));
