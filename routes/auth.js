4bc05cdf66f411e01e0e69130de3606e3a123956
AviationCapitalSim <aviationcapitalsim@gmail.com>
2026-03-21 11:15:07 +0600

    Update auth.js
    await pool.query(`

--- routes/auth.js
+++ routes/auth.js
@@ -134,19 +134,18 @@ router.post("/auth/login", async (req, res) => {
     const userAgent = req.headers["user-agent"] || "";
 
     await pool.query(`
-      INSERT INTO sessions
-      (session_token, token_hash, user_id, airline_id, created_at, expires_at, ip_address, user_agent, active, last_seen_at)
-      VALUES ($1,$2,$3,$4,NOW(),$5,$6,$7,true,NOW())
-    `, [
-      rawToken,            // legacy (no se usará luego)
-      tokenHash,
-      user.user_id,
-      user.airline_id,
-      expiresAt,
-      ip,
-      userAgent
-    ]);
-
+  INSERT INTO sessions
+  (token_hash, user_id, airline_id, created_at, expires_at, ip_address, user_agent, active, last_seen_at)
+  VALUES ($1,$2,$3,NOW(),$4,$5,$6,true,NOW())
+`, [
+  tokenHash,
+  user.user_id,
+  user.airline_id,
+  expiresAt,
+  ip,
+  userAgent
+]);
+     
     // ============================================================
     // RESPONSE
     // ============================================================
