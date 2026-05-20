643886c822c7798c434faf8cd292135a37d367a2
AviationCapitalSim <aviationcapitalsim@gmail.com>
2026-05-20 11:37:23 +0400

    Update import statement for pool from db
    import { pool } from "../db/pool.js";

--- routes/factory.js
+++ routes/factory.js
@@ -12,7 +12,7 @@
    ============================================================ */
 
 import express from "express";
-import pool from "../db/pool.js";
+import { pool } from "../db/pool.js";
 
 const router = express.Router();
 
