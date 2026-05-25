07fd5e9d5b122a2129603701c92fb83ab23e4b6f
AviationCapitalSim <aviationcapitalsim@gmail.com>
2026-05-25 12:52:19 +0400

    Rename manufacturer_multiplier to manufacturing_multiplier
    pr.manufacturing_multiplier,

--- routes/factory.js
+++ routes/factory.js
@@ -60,7 +60,7 @@ router.get("/catalog", async (req, res) => {
         pr.first_delivery_year,
         pr.last_delivery_year,
         pr.capacity_tier,
-        pr.manufacturer_multiplier,
+        pr.manufacturing_multiplier,
         pr.model_weight,
         pr.monthly_min,
         pr.monthly_max
